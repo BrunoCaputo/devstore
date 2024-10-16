615f31bd0a4e076d6292f347e940e93ff089eda9
BrunoCaputo <brunopc.dev@gmail.com>
2024-10-16 13:47:02 -0300

    chore: New eslint and prettier config

--- src/app/(store)/layout.tsx
+++ src/app/(store)/layout.tsx
@@ -1,9 +1,10 @@
-import { Header } from '@/components/header'
 import { PropsWithChildren } from 'react'
+
+import { Header } from '@/components/header'
 
 export default function StoreLayout({ children }: PropsWithChildren) {
   return (
-    <div>
+    <div className="mx-auto grid min-h-screen w-full max-w-[1600px] grid-rows-[min-content_max-content]">
       <Header />
       {children}
     </div>
