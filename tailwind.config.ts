7504ba906ac7da614e7e36d96bdcbd9fdf1b0928
BrunoCaputo <brunopc.dev@gmail.com>
2024-10-16 15:26:20 -0300

    feat: Home page

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from 'tailwindcss'
+import defaultTheme from 'tailwindcss/defaultTheme'
 
 const config: Config = {
   content: [
@@ -12,6 +13,7 @@ const config: Config = {
     },
     gridTemplateRows: {
       app: 'min-content max-content',
+      ...defaultTheme.gridTemplateRows,
     },
   },
   plugins: [],
