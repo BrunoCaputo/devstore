445646355195a8308a8bca7ed0c1a62771448dc8
BrunoCaputo <brunopc.dev@gmail.com>
2024-10-17 19:09:07 -0300

    e2e: Cypress custom commands

--- cypress/e2e/add-product-to-cart.cy.ts
+++ cypress/e2e/add-product-to-cart.cy.ts
@@ -24,7 +24,7 @@ describe('add product to cart', () => {
   })
 
   it('should be able to search for a product and ad it to the cart', () => {
-    cy.get('input[name=q]').type('moletom').parent('form').submit()
+    cy.searchByQuery('moletom')
 
     cy.get('a[href^="product"] > .transition-transform').first().click()
 
